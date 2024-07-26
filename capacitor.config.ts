b2897ca39712203d7d81ea4aef387cbd79b93268
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-07-26 08:30:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'storage-simulator',
+  webDir: 'dist'
+};
+
+export default config;
